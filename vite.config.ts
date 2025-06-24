393098162fe2a7efab77a1f538902fb956ca8558
kevan <kingex1124@gmail.com>
2025-06-24 17:26:34 +0800

    移除 index.html 中的樣式表鏈接註解，並在 vite.config.ts 中設置基本路徑

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/ShelterSupplyQuiz/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
